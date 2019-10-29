28de1a55bd6498993ec0e4b96fb7cfb4c909283f
Renan Correia <renansap@gmail.com>
2019-10-28 22:21:03 -0300

    Ajuste movimento

--- src/mortalclient/MortalClient.java
+++ src/mortalclient/MortalClient.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -22,24 +22,20 @@ import javax.swing.JPanel;
  */
 public class MortalClient extends javax.swing.JFrame implements Runnable {
 
-    Boolean keyRight = false, keyLeft = false, keyUp = false, keyDown = false;
-    public static final int SPEED = 8;
+    private static final long serialVersionUID = 1L;
 
-    /**
-     * @param args the command line arguments
-     */
-    private javax.swing.JLabel lblScore;
+    Thread gameFlowThread;
+    HashMap<String, Player> players = new HashMap<String, Player>();
+    Socket socketPlayer;
+    BufferedReader in;
+    PrintWriter out;
+    boolean key_r = false;
 
     public MortalClient() {
         initComponents();
     }
-//    public static void main(String[] args) {
-//        // TODO code application logic here
-//    }
 
     private void initComponents() {
-
-        lblScore = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -57,42 +53,36 @@ public class MortalClient extends javax.swing.JFrame implements Runnable {
             }
         });
 
-        lblScore.setText("jLabel1");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                        .addGap(347, 347, 347)
-                        .addComponent(lblScore, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(262, Short.MAX_VALUE))
+                .addGap(0, 800, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(lblScore, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(531, Short.MAX_VALUE))
+                .addGap(0, 600, Short.MAX_VALUE)
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
-        // TODO add your handling code here:
+//        player = new Player();
+//        player.setup();
+//        getContentPane().add(player);
 
         connect();
-        repaint();//ler sobre
+        repaint();
         gameFlowThread = new Thread(this);
         gameFlowThread.start();
-    }//GEN-LAST:event_formWindowOpened 
+    }
 
     public void connect() {
         try {
-            s = new Socket("localhost", 8880);
-            in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            out = new PrintWriter(s.getOutputStream(), true);
+            socketPlayer = new Socket("localhost", 8880);
+            in = new BufferedReader(new InputStreamReader(socketPlayer.getInputStream()));
+            out = new PrintWriter(socketPlayer.getOutputStream(), true);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -100,90 +90,45 @@ public class MortalClient extends javax.swing.JFrame implements Runnable {
 
     public void gameFlow() {
         String command;
-        boolean lEsq = false;
-        boolean lDir = true;
         try {
             while (true) {
-
                 command = in.readLine();
                 String data[] = command.split("\\_");
 
                 String id = data[0];
                 String x = data[1];
                 String y = data[2];
-                String lado = data[5];
-                String pontos = data[6];
+                String dir = data[3];
 
-                if (dados.containsKey(id)) {
+                if (!players.containsKey(id)) {
+                    Player p = new Player();
+                    p.x = Integer.parseInt(x);
+                    p.y = Integer.parseInt(y);
 
-                    Player jogador = dados.get(id);
-                    jogador.x = Integer.parseInt(data[1]);
-                    jogador.y = Integer.parseInt(data[2]);
-
-                    //Calcula e mostra placar
-                    String key = dados.keySet().toArray()[0].toString();
-
-                    System.out.println(key);
-
-                    if (key.equals(id)) {
-                        j1 = Integer.parseInt(pontos);
+                    if (dir.equals("true")) {
+                        p.setIconRight();
                     } else {
-                        j2 = Integer.parseInt(pontos);
+                        p.setIconLeft();
                     }
 
-                    lblScore.setText("<html><h3>Jogador 1 : " + j1 + "<br>Jogador 2 : " + j2 + "</html>");
+                    p.setup();
+                    p.move();
 
-//                    if ("R".equals(lado)) {
-//                        jogador.setIconRight();
-//                    } else {
-//                        jogador.setIconLeft();
-//                    }
-                    if (keyRight) {//move direita
-                        jogador.setIconRight();
-                        jogador.x += SPEED;
-                        lDir = true;
-                        lEsq = false;
-                    }
-
-                    if (keyLeft) {//move esquerda
-                        jogador.setIconLeft();
-                        jogador.x -= SPEED;
-                        lEsq = true;
-                        lDir = false;
-                    }
-
-                    if (keyUp) {
-                        jogador.y -= SPEED;
-                    }
-
-                    if (keyDown) {
-                        jogador.y += SPEED;
-                    }
-
-                    if (!(keyDown || keyUp || keyLeft || keyRight)) {
-                        if (lDir) {
-                            jogador.setIconStoppedD();
-                        }
-                        if (lEsq) {
-                            jogador.setIconStoppedE();
-                        }
-                    }
-                    jogador.move();
+                    getContentPane().add(p);
+                    players.put(id, p);
 
                 } else {
-                    Player jogador = new Player();
-                    jogador.setup();
-                    getContentPane().add(jogador);
-                    dados.put(id, jogador);
+                    Player p = players.get(id);
+                    p.x = Integer.parseInt(x);
+                    p.y = Integer.parseInt(y);
 
-                    jogador.x = Integer.parseInt(data[1]);
-                    jogador.y = Integer.parseInt(data[2]);
-//                    if ("R".equals(lado)) {
-//                        jogador.setIconRight();
-//                    } else {
-//                        jogador.setIconLeft();
-//                    }
-                    jogador.move();
+                    if (dir.equals("true")) {
+                        p.setIconRight();
+                    } else {
+                        p.setIconLeft();
+                    }
+
+                    p.move();
                 }
 
             }
@@ -192,91 +137,53 @@ public class MortalClient extends javax.swing.JFrame implements Runnable {
             System.exit(1);
         }
     }
-//         private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
-//        // TODO add your handling code here:
-//        if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {
-//            out.println("PR_R");
-//            
-//        }
-//        if (evt.getKeyCode() == KeyEvent.VK_LEFT) {
-//            out.println("PR_L");
-//            
-//        }
-//        if (evt.getKeyCode() == KeyEvent.VK_UP) {
-//            out.println("PR_U");
-//        }
-//        if (evt.getKeyCode() == KeyEvent.VK_DOWN) {
-//            out.println("PR_D");
-//        }
-//        if (evt.getKeyCode() == KeyEvent.VK_SPACE) {
-//            out.println("PUNCH");
-//        }
-//        
-//    }//GEN-LAST:event_formKeyPressed
 
-    //aplicando
     private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
-        // TODO add your handling code here:
-        switch (evt.getKeyCode()) {
-            case KeyEvent.VK_RIGHT:
-                keyRight = true;
-                break;
-            case KeyEvent.VK_LEFT:
-                keyLeft = true;
-                break;
-            case KeyEvent.VK_UP:
-                keyUp = true;
-                break;
-            case KeyEvent.VK_DOWN:
-                keyDown = true;
-                break;
+
+        if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {
+            out.println("PR_R");
         }
 
-    }//GEN-LAST:event_formKeyPressed
+        if (evt.getKeyCode() == KeyEvent.VK_LEFT) {
+            out.println("PR_L");
+        }
 
-//    private void formKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyReleased
-//        // TODO add your handling code here:
-//        if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {
-//            key_r = false;
-//            out.println("RE_R");
-//
-//        }
-//        if (evt.getKeyCode() == KeyEvent.VK_LEFT) {
-//            key_r = false;
-//            out.println("RE_L");
-//
-//        }
-//        if (evt.getKeyCode() == KeyEvent.VK_UP) {
-//            key_r = false;
-//            out.println("RE_U");
-//        }
-//        if (evt.getKeyCode() == KeyEvent.VK_DOWN) {
-//            key_r = false;
-//            out.println("RE_D");
-//        }
-//    }//GEN-LAST:event_formKeyReleased
-// aplicando
+        if (evt.getKeyCode() == KeyEvent.VK_UP) {
+            out.println("PR_U");
+        }
+
+        if (evt.getKeyCode() == KeyEvent.VK_DOWN) {
+            out.println("PR_D");
+        }
+
+    }
+
     private void formKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyReleased
-        // TODO add your handling code here elias
-        switch (evt.getKeyCode()) {
-            case KeyEvent.VK_RIGHT:
-                keyRight = false;
-                break;
-            case KeyEvent.VK_LEFT:
-                keyLeft = false;
-                break;
-            case KeyEvent.VK_UP:
-                keyUp = false;
-                break;
-            case KeyEvent.VK_DOWN:
-                keyDown = false;
-                break;
+        if (evt.getKeyCode() == KeyEvent.VK_RIGHT) {
+            key_r = false;
+            out.println("RE_R");
         }
-    }//GEN-LAST:event_formKeyReleased
+
+        if (evt.getKeyCode() == KeyEvent.VK_LEFT) {
+            key_r = false;
+            out.println("RE_L");
+        }
+
+        if (evt.getKeyCode() == KeyEvent.VK_UP) {
+            key_r = false;
+            out.println("RE_U");
+        }
+
+        if (evt.getKeyCode() == KeyEvent.VK_DOWN) {
+            key_r = false;
+            out.println("RE_D");
+        }
+
+    }
 
     @Override
     public void paint(Graphics g) {
-        super.paint(g); //To change body of generated methods, choose Tools | Templates.
+        super.paint(g);
     }
 
     @Override
@@ -284,16 +191,7 @@ public class MortalClient extends javax.swing.JFrame implements Runnable {
         gameFlow();
     }
 
-    /**
-     *
-     * @param args
-     */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -310,22 +208,11 @@ public class MortalClient extends javax.swing.JFrame implements Runnable {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(MortalClient.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new MortalClient().setVisible(true);
             }
         });
     }
-
-    Thread gameFlowThread;
-    HashMap<String, Player> dados = new HashMap<String, Player>();
-    Socket s;
-    BufferedReader in;
-    PrintWriter out;
-    boolean key_r = false;
-    int j1 = 0, j2 = 0;
 
 }
--- src/mortalclient/Player.java
+++ src/mortalclient/Player.java
@@ -1,35 +1,100 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package mortalclient;
+//
+//import java.awt.Image;
+//import javax.swing.ImageIcon;
+//import javax.swing.JLabel;
+//
+///**
+// *
+// * @author Ksa
+// */
+//public class Player extends JLabel {
+//
+//    public int x = 0, y = 0, id, pontos;
+//    public char lado = 'R';
+//    ImageIcon walkL;
+//    ImageIcon walkR;
+//    ImageIcon stoppedD; //Parada Direita
+//    ImageIcon stoppedE;//Parada Esquerda
+//
+//       public void setup() {
+//        setText("12");
+//
+//       stoppedD = new ImageIcon(new ImageIcon(getClass().getResource("p_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//        stoppedE = new ImageIcon(new ImageIcon(getClass().getResource("p_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//        walkR = new ImageIcon(new ImageIcon(getClass().getResource("c_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//        walkL = new ImageIcon(new ImageIcon(getClass().getResource("c_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//
+//        setBounds(x, y, 90, 127);
+//        setIcon(walkR);
+//    }
+//
+//    public void move() {
+//
+////
+////        System.out.println("******************************************");
+////        System.out.println(x);
+////        System.out.println(y);
+//        setBounds(x, y, 90, 127);
+//    }
+////
+////    public void setIconRight() {;
+////        setIcon(walkR);
+////    }
+////
+////    public void setIconLeft() {
+////        setIcon(walkL);
+////    }
+//
+//
+//    //implementando
+//     public void setIconRight() {
+//        setIcon(walkR);
+//    }
+//
+//    public void setIconLeft() {
+//        setIcon(walkL);
+//    }
+//
+//    public void setIconStoppedD() {
+//        setIcon(stoppedD);
+//    }
+//
+//    public void setIconStoppedE() {
+//        setIcon(stoppedE);
+//    }
+//
+//}
 package mortalclient;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-/**
- *
- * @author Ksa
- */
 public class Player extends JLabel {
 
-    public int x = 0, y = 0, id, pontos;
-    public char lado = 'R';
+    public int x = 50, y = 50;
+
     ImageIcon walkL;
     ImageIcon walkR;
-    ImageIcon stoppedD; //Parada Direita
-    ImageIcon stoppedE;//Parada Esquerda
+    ImageIcon walkU;
+    ImageIcon walkD;
+//    ImageIcon stoppedD; //Parada Direita
+//    ImageIcon stoppedE;//Parada Esquerda
 
-       public void setup() {
+    public void setup() {
         setText("12");
-
-        stoppedD = new ImageIcon(new ImageIcon(getClass().getResource("p_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
-        stoppedE = new ImageIcon(new ImageIcon(getClass().getResource("p_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
         walkR = new ImageIcon(new ImageIcon(getClass().getResource("c_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
         walkL = new ImageIcon(new ImageIcon(getClass().getResource("c_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
-
+//        stoppedD = new ImageIcon(new ImageIcon(getClass().getResource("p_d.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//        stoppedE = new ImageIcon(new ImageIcon(getClass().getResource("p_e.gif")).getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//        walkR = new ImageIcon(new ImageIcon("./walk_r.gif").getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
+//        walkL = new ImageIcon(new ImageIcon("./walk_l.gif").getImage().getScaledInstance(88, 127, Image.SCALE_DEFAULT));
         setBounds(x, y, 90, 127);
         setIcon(walkR);
     }
@@ -38,30 +103,11 @@ public class Player extends JLabel {
         setBounds(x, y, 90, 127);
     }
 
-//    public void setIconRight() {
-//        setIcon(walkR);
-//    }
-//
-//    public void setIconLeft() {
-//        setIcon(walkL);
-//    }
-    
-    
-    //implementando
-     public void setIconRight() {
+    public void setIconRight() {
         setIcon(walkR);
     }
 
     public void setIconLeft() {
         setIcon(walkL);
     }
-
-    public void setIconStoppedD() {
-        setIcon(stoppedD);
-    }
-
-    public void setIconStoppedE() {
-        setIcon(stoppedE);
-    }
-
 }
